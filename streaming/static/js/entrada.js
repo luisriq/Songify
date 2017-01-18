f99116bbdd8d81fdf17c04122da3557652f9fb30
firenessmax <firenessmax@gmail.com>
2017-01-18 17:22:33 -0300

    cambios
    va a quedar la cagá

--- streaming/static/js/entrada.js
+++ streaming/static/js/entrada.js
@@ -1,35 +1,36 @@
 var timerCargarAudio = null;
+var timerHighlight = null;
 var entrada = $("#userInput");
+
+var funHighlight = function () {
+	$(".editable span.mark").removeClass('mark');
+	var len = $(".editable span").length;
+	var i = Math.floor(Math.random()*len);
+	$($(".editable span")[i]).addClass('mark');
+	var j = Math.floor(Math.random()*5);
+	if(j==0&&$($(".editable span")[i]).text().length>3){
+		timerCargarAudio =  setTimeout(nuevoAudio, 2000);
+	}else{
+		timerHighlight =  setTimeout(funHighlight, 500);
+	}
+}
+
 $("#userInput").keyup(
 	function(){
+		pause();
 		if(timerCargarAudio != null){
 			clearTimeout(timerCargarAudio);
 		}
 		//TODO: Random 2-5 segs
 		//TODO: Mostrar carga
-		timerCargarAudio =  setTimeout(nuevoAudio, 2000);
-
+		
 
 		//BUSCAR
-		var pk = "wea"
-		if(entrada.html().search(pk+"(?!<\/span>)") >= 0){
-			//Agregar SPAN
-			console.log("->>>")
-			var ind1 = entrada.html().search(pk+"(?!<\/span>)")
-			var ind2 = ind1 + pk.length
-			var a1 = entrada.html().substr(0,ind1) + "<span>"+pk+"</span>&nbsp;" + entrada.html().substr(ind2);
-			entrada.html(a1);
-			//Cursor
-
-	        var range = document.createRange();
-	        range.selectNodeContents(document.getElementById("userInput").childNodes[0], ind1+(("<span>"+pk+"</span>&nbsp;").length+10));
-	        range.collapse(false);
-	        var sel = window.getSelection();
-	        sel.removeAllRanges();
-	        sel.addRange(range);
-
-			//entrada.html(entrada.html().replace(pk+"(?!<\/span>)", "<span>"+pk+"</span>"))
+		
+		if(timerHighlight != null){
+			clearTimeout(timerHighlight);
 		}
+		timerHighlight =  setTimeout(funHighlight, 500);
 });
 
 
