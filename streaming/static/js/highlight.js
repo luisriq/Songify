c9522753150022af7d57f23b42b4f7d903ba09f7
firenessmax <firenessmax@gmail.com>
2017-01-18 18:41:47 -0300

    que hiciste lucho noooooo

--- streaming/static/js/highlight.js
+++ streaming/static/js/highlight.js
@@ -1,43 +1,139 @@
 (function($){
-	$.fn.highlight=function(parametros){
-		pars=$.extend({
-
-		},parametros)
-
+	$.fn.mysticEqualizer=function(parametros){
 		var self = $(this);
-		self.css("color","rgba(0,0,0,.5)");
-		self.css("background","transparent");
-		var padre = self.parent();
 
+		self.pars=$.extend({
+			width:"100%",
+			height:"60px",
+			maxBarHeight : 100,//30,
+			minBarHeight : 3,//15
+			barPadding : 2,//space between bars
+			barWidth : 2,//width of each bar
+			totalBars :100,//total bars to create
+			barColor : '#fff',
+			fps : 10
+		},parametros)
+		self.playState = false;
+		//crear canvas
+		var canvas = $('<canvas width="300" height="100">Your browser does not support the HTML5 canvas tag.</canvas>')[0];
+		self.parent().find("canvas").remove();
+		$(canvas)
+			.height(self.pars.height)
+			.width(self.pars.width);
 
-		self.contenedor = $("<div></div>");
-		self.contenedor.height(self.height());
-		self.contenedor.css("padding", "7px");
-		self.contenedor.css("width","100%");
-		//self.contenedor.css("overflow-x", "hidden");
-		padre.prepend(self.contenedor);
-		//self.contenedor.css("float", "left");
-		
+		self.prepend(canvas);
+		var ctx = canvas.getContext("2d");
+		var bar = [];
+		self.play=function(){
+			self.playState = true;
+			var barHeight = 0;
+		  	var x = 0;
+		  	var y = 0;
+			for(var i=0; i<self.pars.totalBars; i++)
+		    {
+		      	barHeight = self.pars.minBarHeight;//self.RandomizeHeight();
+		      	x = (i + i) * self.pars.barPadding;
+		        y = self.ComputeYAxis(barHeight);
+		        bar[i]={
+		          height: barHeight,
+		          buffHeight: barHeight,
+		          x: x,
+		          y: y
+		      	};
+		      	self.GrowBar(i);
+		    }
+		}
+		self.stop = function(){
+			self.playState = false;
+		}
+		self.InitBars = function()
+		{
+		    var barHeight = 0;
+		  	var x = 0;
+		  	var y = 0;
+		  	for(var i=0; i<self.pars.totalBars; i++)
+		    {
+		      	barHeight = self.pars.minBarHeight;//self.RandomizeHeight();
+		      	x = (i + i) * self.pars.barPadding;
+		        y = self.ComputeYAxis(barHeight);
+		      	bar.push({
+		          height: barHeight,
+		          buffHeight: barHeight,
+		          x: x,
+		          y: y
+		      	});
+		      	self.GrowBar(i);
+		    }
 
-		self.css("margin-top", (-1*(self.height()))+"px");
-		self.css("display", "block");
-		//padre.prepend();
-		self.keyup(function(){
-			self.contenedor.html(self.val());
-		});
-		/*self.keypress(function(ev){
-			self.contenedor.html(self.val()+String.fromCharCode(ev.which));
-
-		});*/
-
-
+		}
+		self.ShrinkBar = function(index)
+		{
+		  	if(bar[index].height > self.pars.minBarHeight)
+		   	{
+		      	ctx.fillStyle = self.pars.barColor;
+		      	ctx.clearRect(bar[index].x, bar[index].y, self.pars.barWidth, bar[index].height);
+		      	var decrecimiento = Math.floor(Math.random()*5);
+		        bar[index].height-=decrecimiento;
+		        if(bar[index].height<self.pars.minBarHeight)
+		        	bar[index].height = self.pars.minBarHeight;
+		      	bar[index].y = self.ComputeYAxis(bar[index].height);
+		        ctx.fillRect(bar[index].x, bar[index].y, self.pars.barWidth, bar[index].height);
+		     // 	setTimeout(function() {
+	     	
+		        requestAnimationFrame(function(){
+		      		self.ShrinkBar(index)
+		    			})
+		    	//	}, 1000 / self.pars.fps);
+		    }
+		  	else
+		    {
+		      	var newHeight = self.RandomizeHeight();
+		      	while(newHeight==self.pars.minBarHeight)
+		        {
+		            newHeight = self.RandomizeHeight();
+		        }
+		      	bar[index].buffHeight = newHeight;
+		      	if(self.playState)
+		      		self.GrowBar(index);
+		    }
+		   	
+		}
+		self.GrowBar = function(index)
+		{
+		  	if(bar[index].height < bar[index].buffHeight)
+		   	{
+		      	ctx.fillStyle = self.pars.barColor;
+		      	ctx.clearRect(bar[index].x, bar[index].y, self.pars.barWidth, bar[index].height);
+		      	var crecimiento = Math.floor(Math.random()*5);
+		        bar[index].height+=crecimiento;
+		      	bar[index].y = self.ComputeYAxis(bar[index].height);
+		        ctx.fillRect(bar[index].x, bar[index].y, self.pars.barWidth, bar[index].height);
+		      //	setTimeout(function() {
+		      	if(self.playState)
+			        requestAnimationFrame(function(){
+			            self.GrowBar(index)
+			          })
+			    else
+			    	self.ShrinkBar(index);
+		    	//	}, 1000 / fps);
+		    }
+		  	else
+		    {
+		      	self.ShrinkBar(index);//shrink it again
+		    }
+		   
+		}
+		self.RandomizeHeight = function()//generate random seed number
+		{
+		  	return Math.floor((Math.random() * self.pars.maxBarHeight) + self.pars.minBarHeight);
+		}
+		self.ComputeYAxis = function(height)//computes the position of the y
+		{
+		   return Math.round((self.pars.maxBarHeight - height)/2);
+		}
+		self.InitBars();
 		return self;
 
 	}
-}(jQuery));
+})(jQuery)
 
-
-
-
-var h = $("input").highlight();
-console.log(h)
